10aa589bd23e32e376766813510cfc78f0718467
Jason Huang <jason_huang@linkwish.com>
2024-04-24 00:46:50 +0800

    test

--- firebase.py
+++ firebase.py
@@ -2,12 +2,12 @@ import base64
 import json
 import os
 from datetime import datetime
-from enum import Enum
 
 import firebase_admin
 from firebase_admin import credentials, firestore
 
 from artist import ARTIST
+from sns_type import SnsType
 
 
 class Firebase:
@@ -34,3 +34,40 @@ class Firebase:
         decoded_bytes = base64.b64decode(key)
         decoded_string = decoded_bytes.decode('utf-8')
         return json.loads(decoded_string)
+
+    def set_updated_at(self, type: SnsType, id: str, updated_at: datetime):
+        doc_ref = self.__db.collection(type.value).document(id)
+        doc_ref.update({
+            "updated_at": updated_at
+        })
+
+    def base64_decode(self, key) -> dict:
+        decoded_bytes = base64.b64decode(key)
+        decoded_string = decoded_bytes.decode('utf-8')
+        return json.loads(decoded_string)
+
+    def add_account(self, type: SnsType, id: str, username: str, discord_channel_id: str, updated_at: datetime):
+        doc_ref = self.__db.collection(type.value).document(id)
+        data = {
+            "username": username,
+            "discord_channel_id": discord_channel_id,
+            "updated_at": updated_at
+        }
+        doc_ref.set(data)
+
+    def get_documents(self, type: SnsType):
+        return self.__db.collection(type.value).stream()
+
+    def is_account_exists(self, type: SnsType, id: str):
+        doc_ref = self.__db.collection(type.value).document(id)
+        return doc_ref.get().exists
+
+    def delete_account(self, type: SnsType, id: str):
+        return self.__db.collection(type.value).document(id).delete()
+
+    def get_subscribed_list(self, type: SnsType):
+        return self.__db.collection(type.value).stream()
+
+    def get_subscribed_list_from_discord_id(self, type: SnsType, discord_id: str):
+        docs = self.__db.collection(type.value).stream()
+        return [(doc.get("username"), doc.id) for doc in docs if doc.get("discord_channel_id") == discord_id]
--- main.py
+++ main.py
@@ -1,4 +1,28 @@
-from el7zup_bot import EL7ZUPBot
+import os
 
-bot = EL7ZUPBot()
-bot.execute()
+import discord
+
+from bstage_bot import BstageBot
+from el7zup_bot import EL7ZUPBot
+from dotenv import load_dotenv
+
+from firebase import Firebase
+
+load_dotenv()
+firebase = Firebase()
+el7zup_bot = EL7ZUPBot(firebase)
+el7zup_bot.execute()
+bot = discord.Bot()
+
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name} ({bot.user.id})')
+    print('Bot is ready to receive commands')
+    bstage_bot = BstageBot(bot, firebase)
+    await bstage_bot.execute()
+    await bot.close()
+    await exit()
+
+
+bot.run(os.environ["BOT_TOKEN"])
